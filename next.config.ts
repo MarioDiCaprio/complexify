e053a18e68160f74e191e155ee55fd5b01633ef1
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-04-10 12:40:33 +0200

    Added support for loading GLSL scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
+import {load} from "signal-exit";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => ({
+        externals: Object.assign({}, config.externals, {
+            fs: 'fs',
+        }),
+        module: Object.assign({}, config.module, {
+            // loader for GLSL files
+            rules: config.module.rules.concat([
+                {
+                    test: /\.(glsl|vs|fs|vert|frag)$/,
+                    exclude: /node_modules/,
+                    use: ['raw-loader', 'glslify-loader']
+                }
+            ]),
+        }),
+    }),
+    experimental: {
+        turbo: {
+            rules: {
+                // loader for GLSL files
+                '*.glsl': {
+                    loaders: ['raw-loader', 'glslify-loader'],
+                    as: '*.js'
+                }
+            },
+        }
+    }
 };
 
 export default nextConfig;
