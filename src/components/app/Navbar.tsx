5dcb9cdd977380cd5a32ab72ba1b91ef31d40a15
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-04-19 17:44:37 +0200

    Added logo

--- src/components/app/Navbar.tsx
+++ src/components/app/Navbar.tsx
@@ -2,13 +2,22 @@
 
 import React from "react";
 import {Link, Navbar as HeroUiNavbar, NavbarBrand, NavbarContent, NavbarItem} from "@heroui/react";
+import Image from "next/image";
+
+import complexifyBrandImg from "../../../public/img/logo/complexify-brand-black.webp";
 
 
 const Navbar: React.FC = () => {
     return (
         <HeroUiNavbar height="3rem" className="fixed w-full bg-background flex items-center justify-between">
-            <NavbarBrand>
-                <span>Complexify</span>
+            <NavbarBrand className="h-full">
+                <Link href="/" className="h-full">
+                    <Image
+                        src={complexifyBrandImg}
+                        alt="Complexify"
+                        className="h-full w-auto p-2 select-none"
+                    />
+                </Link>
             </NavbarBrand>
             <NavbarContent justify="center" className="flex items-center gap-12">
                 <NavbarItem>
