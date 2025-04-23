6b83f0b865c2c556ddae0a8e45d811a5ba120243
Mario Di Caprio <mariodicaprio10@gmail.com>
2025-04-23 12:07:26 +0200

    Improved error handling on small screens

--- src/components/app/(graphing)/EquationsErrorHandler.tsx
+++ src/components/app/(graphing)/EquationsErrorHandler.tsx
@@ -9,8 +9,8 @@ const EquationsErrorHandler: React.FC<{ children: ReactNode }> = ({ children }) 
 
     if (error) {
         return (
-            <div className="fixed top-0 left-0 w-screen h-screen p-5 flex justify-end items-end">
-                <div>
+            <div className="fixed top-0 left-0 w-screen h-screen p-5 pt-[6rem] flex justify-end items-start md:items-end">
+                <div className="w-full md:w-fit">
                     <Alert
                         color="danger"
                         title={error.message}
